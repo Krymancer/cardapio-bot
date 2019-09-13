bb2906d88d5a21f599af7e01046b1923ba882e03
Júnior Nascimento <junior.nascm@gmail.com>
2019-09-13 00:22:14 -0300

    returing disher by day

--- Utils.py
+++ Utils.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 from datetime import datetime
 
 
-def getMenu(date):
+def getMenu():
     r = requests.get('http://www.sobral.ufc.br/ru/cardapio/')
     soup = BeautifulSoup(r.text, 'html.parser')
     tables = soup.find_all('tbody')
@@ -24,15 +24,27 @@ def getMenu(date):
         cols = [ele.text.strip() for ele in cols]
         jantar_data.append([ele for ele in cols if ele])
 
-    # for rows in almoco_data:
-    #     print(rows)
-    #
-    # for rows in jantar_data:
-    #     print(rows)
-
     return almoco_data, jantar_data
+
+def getTodayDishes(date):
+    almoco,jantar = getMenu()
+    almoco_dishes = []
+    jantar_dishes = []
+
+    i = -1
+    for index,col in enumerate(almoco[0]):
+        if col == date:
+            i = index
+
+    for dishes in almoco[1:]:
+        almoco_dishes.append(dishes[i])
+
+    for dishes in jantar[1:]:
+        jantar_dishes.append(dishes[i])
+
+    return almoco_dishes,jantar_dishes
 
 
 def getDateHour():
     today = datetime.today()
-    return today.strftime('%d/%m'), today.strftime('%H:%M')
+    return today.strftime('%d/%m')#, today.strftime('%H:%M')
