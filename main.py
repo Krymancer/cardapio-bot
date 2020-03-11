f141241be1f46ac2c7f5b4cafd58d7401c9a58f2
Lucas Santos <lucas26xd@gmail.com>
2020-03-11 20:29:55 -0300

    Fix botões com emojis

--- main.py
+++ main.py
@@ -28,7 +28,9 @@ def on_chat_message(msg):
                     users.append(chat_id)
                     # Aqui haveria uma parte de persistência do chat_id, assumindo que o user deseja receber as notificações
             elif 'almoco' in command.lower().replace('ç', 'c') or 'janta' in command.lower():  # TA para entrada pelos botões customizados
-                send_menu(chat_id, demojize(command).lower().replace('ç', 'c').replace('/', '')[command.rfind(':')+1:].strip())
+                command = demojize(command).lower()
+                command = command[command.rfind(':')+1:]  # Retira código emoji
+                send_menu(chat_id, command.replace('ç', 'c').replace('/', '').strip())
             else:
                 send_message(chat_id, ':disappointed_relieved: Desculpe, não entendi o que você me falou.')
         else:  # Caso seja enviado mensagens de tipos diferentes de 'text'
